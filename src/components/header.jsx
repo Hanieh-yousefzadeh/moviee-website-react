3a1650815d5fe6a01b3b090014af8050494033bd
hanieh-y <haniye.rooshan1377@gmail.com>
2026-09-12 02:00:43 +0330

    feat:show genre

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,11 @@
-function Header(){
-    return(
-        <header>header</header>
+import { Link } from "react-router";
+function Header() {
+    return (
+        <header>
+            <Link to="genre/Drama">Drama</Link>
+            <Link to="genre/Comedy">Comedy</Link>
+            <Link to="genre/Ation">Action</Link>
+        </header>
     )
 };
 export default Header;
--- src/pages/genre.jsx
+++ src/pages/genre.jsx
@@ -1,6 +1,31 @@
-function Genre(){
-    return(
-        <h1>Genre Page</h1>
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import ShowCart from "../components/showCart";
+function Genre() {
+
+    const [shows, setShows] = useState([]);
+    const { genre } = useParams();
+    console.log(genre)
+
+    useEffect(() => {
+        async function fetchShows() {
+            const response = await fetch("https://api.tvmaze.com/shows?page=1")
+            const data = await response.json();
+            setShows(data);
+            console.log(data[0].genres);
+        }
+
+        fetchShows();
+    }, [])
+    const filter = shows.filter((show) => (
+        show.genres.includes(genre)
+    ));
+    console.log(filter)
+    return (
+        <div>
+            <h1>{genre}</h1>
+            {filter.map((show) => (<ShowCart key={show.id} show={show} />))}
+        </div>
     )
 };
 export default Genre;
