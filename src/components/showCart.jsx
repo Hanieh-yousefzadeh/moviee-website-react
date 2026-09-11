2a7ea9d416325daf057593d809ae814050ba0724
hanieh-y <haniye.rooshan1377@gmail.com>
2026-09-12 01:01:18 +0330

    feat: add show card

--- src/components/showCart.jsx
+++ src/components/showCart.jsx
@@ -1,6 +1,13 @@
-function ShowCart(){
-    return(
-        <p>cart</p>
+import { Link } from "react-router";
+function ShowCart({ show }) {
+    return (
+        <Link to={`/show/${show.id}`}>
+
+            <img src={show.image} alt="" />
+            <h3>{show.name}</h3>
+            <p>{show.premiered}</p>
+            <span>{show.rating.average}</span>
+        </Link>
     )
 };
 export default ShowCart;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,21 +1,27 @@
-import { useState,useEffect } from "react";
-function Home(){
+import { useState, useEffect } from "react";
+import ShowCart from "../components/showCart";
+function Home() {
 
-    const [shows, setShows]= useState([]);
+    const [shows, setShows] = useState([]);
 
-    useEffect (()=>{
-        async function fetchShows(){
-            const response = await fetch( "https://api.tvmaze.com/shows?page=1")
+    useEffect(() => {
+        async function fetchShows() {
+            const response = await fetch("https://api.tvmaze.com/shows?page=1")
             const data = await response.json();
             setShows(data)
         }
 
         fetchShows();
-    },[])
+    }, [])
 
     console.log(shows);
-    return(
-        <h1>Home Page</h1>
+    return (
+        <div>
+            <h1>Home Page</h1>
+            {shows.map((show) => (
+                <ShowCart key={show.id} show={show} />
+            ))}
+        </div>
     )
 };
 export default Home;
