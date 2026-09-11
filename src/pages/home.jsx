4afa692c0605bf6fb854ae560610130e36f7c8b5
hanieh-y <haniye.rooshan1377@gmail.com>
2026-09-12 00:01:55 +0330

    feat: get api

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,19 @@
+import { useState,useEffect } from "react";
 function Home(){
+
+    const [shows, setShows]= useState([]);
+
+    useEffect (()=>{
+        async function fetchShows(){
+            const response = await fetch( "https://api.tvmaze.com/shows?page=1")
+            const data = await response.json();
+            setShows(data)
+        }
+
+        fetchShows();
+    },[])
+
+    console.log(shows);
     return(
         <h1>Home Page</h1>
     )
