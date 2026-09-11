f440d3e47ce24bed4086b35008da9fda974a965a
hanieh-y <haniye.rooshan1377@gmail.com>
2026-09-12 01:21:22 +0330

    feat: show card in shows.jsx

--- src/pages/shows.jsx
+++ src/pages/shows.jsx
@@ -1,6 +1,29 @@
-function Shows(){
-    return(
-        <h1>Shows Page</h1>
-    )
+import { useEffect, useState } from "react";
+import ShowCart from "../components/showCart";
+
+function Shows() {
+   
+        const [shows, setShows] = useState([]);
+
+     useEffect(() => {
+        async function fetchShows() {
+            const response = await fetch("https://api.tvmaze.com/shows?page=1")
+            const data = await response.json();
+            setShows(data)
+        }
+
+        fetchShows();
+    }, [])
+
+    return (
+        <div>
+            <h1>همه سریال‌ها</h1>
+
+            {shows.map((show) => (
+                <ShowCart key={show.id} show={show} />
+            ))}
+        </div>
+    );
+   
 };
 export default Shows;
