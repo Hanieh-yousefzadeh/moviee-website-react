21860f8372558b2a87dbe6d8cbfef12dd2a19781
hanieh-y <haniye.rooshan1377@gmail.com>
2026-09-14 02:04:50 +0330

    fix : genre rout relative

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,10 +5,10 @@ function Header() {
         <header className="flex justify-between">
 
             <div className="flex">
-                <h1>Moviee</h1>
-                <Link to="genre/drama">Drama</Link>
-                <Link to="genre/Comedy">Comedy</Link>
-                <Link to="genre/Action">Action</Link>
+                <Link to="/">Moviee</Link>
+                <Link to="/genre/drama">Drama</Link>
+                <Link to="/genre/Comedy">Comedy</Link>
+                <Link to="/genre/Action">Action</Link>
             </div>
             <div className="flex gap-5">
                  <Link to="/search" className="flex"><Search /> Search</Link>
